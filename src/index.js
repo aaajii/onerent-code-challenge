eacf78203e52631f12306878d35a31309ebc3e98
Aji <jwpbanawan@gmail.com>
2021-04-21 14:07:27 +0800

    [ADD] Installed and added apollo and graphql libraries

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Importing Apollo libraries
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// Setting URL variable
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// Initializing Apollo Client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
