970d089f4619c8edbbca3b0220168a3958fb21a0
Aji <jwpbanawan@gmail.com>
2021-04-23 01:46:14 +0800

    [FIX] Removed extra header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,16 +14,6 @@ const Header = () => {
           new
         </Link>
         <div className="ml1">|</div>
-        <Link to="/top" className="ml1 no-underline black">
-          top
-        </Link>
-        <div className="ml1">|</div>
-        <Link
-          to="/search"
-          className="ml1 no-underline black"
-        >
-          search
-        </Link>
         <div className="ml1">|</div>
         <Link to="/search" className="ml1 no-underline black">
           search
