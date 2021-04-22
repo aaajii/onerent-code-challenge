d48b0f0a2277b6aa3dbff2bc1c5c1a6b2893b8b0
Aji <jwpbanawan@gmail.com>
2021-04-22 11:09:08 +0800

    [ADD] Added apollo middleware

--- src/index.js
+++ src/index.js
@@ -12,15 +12,28 @@ import {
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
+
+import { AUTH_TOKEN } from './constants';
 
 // Setting URL variable
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}` : ''
+    }
+  }
+});
+
 // Initializing Apollo Client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
