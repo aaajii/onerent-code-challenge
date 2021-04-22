88f8941f6f61309a14d399fb4b147363678621d4
Aji <jwpbanawan@gmail.com>
2021-04-22 10:47:31 +0800

    [ADD] Added Authentication signup and login

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CreateLink from './CreateLink';
 import Header from './Header';
 import LinkList from './LinkList';
+import Login from "./Login";
 import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
             path="/create"
             component={CreateLink}
           />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     </div>
